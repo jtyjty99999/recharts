23a809dc3966a80527f8c2584653688c6cdb70a1
jtyjty99999 <jtyjty99999@126.com>
2017-02-10 14:44:58 +0800

    feat:support show all ticks

---
+++ test/specs/component/CellSpec.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { mount, render } from 'enzyme';
+import { expect } from 'chai';
+import { Surface, Cell } from 'recharts';
+
+describe('<Cell />', () => {
+  it('Render empty dom', () => {
+    const wrapper = mount(
+      <Cell />
+    );
+
+    expect(wrapper.props().children).to.be.empty;
+  });
+});
